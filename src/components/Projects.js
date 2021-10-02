e84e51def6c37b78ed3e2f38b28f44fc46211c18
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-10-02 15:39:53 -0700

    changed all occurences of class to className in projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,6 +3,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 import Nav from 'react-bootstrap/Nav';
+import Card from 'react-bootstrap/Card'
 
 export default function Projects() {
     return (
@@ -19,50 +20,50 @@ export default function Projects() {
                 </Row>
             </Container>
 
-            <div class="album py-5 bg-secondary">
-                <div class="container">
-                    <div class="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-3">
+            <div className="album py-5 bg-secondary">
+                <Container>
+                    <Row className="row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-3">
 
                         {/* Cup of Sugear */}
-                        <div class="col">
-                            <div class="card shadow position-relative">
-                                <img src="./assets/images/cupofsugear.png" class="bd-placeholder-img card-img-top"
+                        <Col>
+                            <div className="card shadow position-relative">
+                                <img src="./assets/images/cupofsugear.png" className="bd-placeholder-img card-img-top"
                                     width="100%" height="225" alt="Cup of Sugar Thumbnail" />
 
-                                <div class="card-body">
-                                    <p class="card-text"><strong>Cup of Sugear</strong> - A tool to help neighbors
+                                <div className="card-body">
+                                    <p className="card-text"><strong>Cup of Sugear</strong> - A tool to help neighbors
                                         borrow/loan/exchange items.</p>
-                                    <p class="card-text"><strong>Technologies Used</strong> - HTML, CSS, JavaScript,
+                                    <p className="card-text"><strong>Technologies Used</strong> - HTML, CSS, JavaScript,
                                         Node.js, Express.js, MySQL, Sequelize, Handlebars</p>
-                                    <div class="d-flex justify-content-center align-items-center">
-                                        <div class="btn-group">
+                                    <div className="d-flex justify-content-center align-items-center">
+                                        <div className="btn-group">
                                             <a href="https://cup-of-sugear.herokuapp.com/" target="_blank"
-                                                class="btn btn-sm btn-outline-secondary">View</a>
+                                                className="btn btn-sm btn-outline-secondary">View</a>
                                             <a href="https://github.com/AdrianCronin/Cup-of-Sugear" target="_blank"
-                                                class="btn btn-sm btn-outline-secondary">Repo</a>
+                                                className="btn btn-sm btn-outline-secondary">Repo</a>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </Col>
 
                         {/* Tech Blog */}
-                        <div class="col">
-                            <div class="card shadow position-relative">
-                                <img src="./assets/images/techblog.jpg" class="bd-placeholder-img card-img-top" width="100%"
+                        <div className="col">
+                            <div className="card shadow position-relative">
+                                <img src="./assets/images/techblog.jpg" className="bd-placeholder-img card-img-top" width="100%"
                                     height="225" alt="Tech Blog Thumbnail" />
 
-                                <div class="card-body">
-                                    <p class="card-text"><strong>Tech Blog</strong> - A blog site where users can
+                                <div className="card-body">
+                                    <p className="card-text"><strong>Tech Blog</strong> - A blog site where users can
                                         view/publish blog posts and comment on other posts.</p>
-                                    <p class="card-text"><strong>Technologies Used</strong> - HTML, CSS, JavaScript,
+                                    <p className="card-text"><strong>Technologies Used</strong> - HTML, CSS, JavaScript,
                                         Node.js, Express.js, MySQL, Sequelize, Handlebars</p>
-                                    <div class="d-flex justify-content-center align-items-center">
-                                        <div class="btn-group">
+                                    <div className="d-flex justify-content-center align-items-center">
+                                        <div className="btn-group">
                                             <a href="https://a-tech-discourse.herokuapp.com/" target="_blank"
-                                                class="btn btn-sm btn-outline-secondary">View</a>
+                                                className="btn btn-sm btn-outline-secondary">View</a>
                                             <a href="https://github.com/AdrianCronin/Tech-Blog" target="_blank"
-                                                class="btn btn-sm btn-outline-secondary">Repo</a>
+                                                className="btn btn-sm btn-outline-secondary">Repo</a>
                                         </div>
                                     </div>
                                 </div>
@@ -70,22 +71,22 @@ export default function Projects() {
                         </div>
 
                         {/* Employee Management System */}
-                        <div class="col">
-                            <div class="card shadow position-relative">
-                                <img src="./assets/images/ems_menu.jpg" class="bd-placeholder-img card-img-top" width="100%"
+                        <div className="col">
+                            <div className="card shadow position-relative">
+                                <img src="./assets/images/ems_menu.jpg" className="bd-placeholder-img card-img-top" width="100%"
                                     height="225" alt="Employee Management System Thumbnail" />
 
-                                <div class="card-body">
-                                    <p class="card-text"><strong>Employee Management System</strong> - A command line
+                                <div className="card-body">
+                                    <p className="card-text"><strong>Employee Management System</strong> - A command line
                                         application to help manage a company's employee database.</p>
-                                    <p class="card-text"><strong>Technologies Used</strong> - JavaScript, Node.js, MySQL,
+                                    <p className="card-text"><strong>Technologies Used</strong> - JavaScript, Node.js, MySQL,
                                         Inquirer</p>
-                                    <div class="d-flex justify-content-center align-items-center">
-                                        <div class="btn-group">
+                                    <div className="d-flex justify-content-center align-items-center">
+                                        <div className="btn-group">
                                             <a href="https://drive.google.com/file/d/1SnPjHrLkBnNkWfSVr36kgNF94oEn0KgC/view"
-                                                target="_blank" class="btn btn-sm btn-outline-secondary">Demo</a>
+                                                target="_blank" className="btn btn-sm btn-outline-secondary">Demo</a>
                                             <a href="https://github.com/AdrianCronin/Employee-Management-System"
-                                                target="_blank" class="btn btn-sm btn-outline-secondary">Repo</a>
+                                                target="_blank" className="btn btn-sm btn-outline-secondary">Repo</a>
                                         </div>
                                     </div>
                                 </div>
@@ -93,22 +94,22 @@ export default function Projects() {
                         </div>
 
                         {/* The Collector */}
-                        <div class="col">
-                            <div class="card shadow position-relative">
-                                <img src="./assets/images/screenshot3.png" class="bd-placeholder-img card-img-top"
+                        <div className="col">
+                            <div className="card shadow position-relative">
+                                <img src="./assets/images/screenshot3.png" className="bd-placeholder-img card-img-top"
                                     width="100%" height="225" alt="The Collector Thumbnail" />
 
-                                <div class="card-body">
-                                    <p class="card-text"><strong>The Collector</strong> - A tool to help Marvel fans
+                                <div className="card-body">
+                                    <p className="card-text"><strong>The Collector</strong> - A tool to help Marvel fans
                                         discover new Collectibles, Comics, Storylines and Characters!</p>
-                                    <p class="card-text"><strong>Technologies Used</strong> - HTML, CSS, JavaScript, Bulma,
+                                    <p className="card-text"><strong>Technologies Used</strong> - HTML, CSS, JavaScript, Bulma,
                                         jQuery</p>
-                                    <div class="d-flex justify-content-center align-items-center">
-                                        <div class="btn-group">
+                                    <div className="d-flex justify-content-center align-items-center">
+                                        <div className="btn-group">
                                             <a href="https://michellecoder.github.io/The-Collector/" target="_blank"
-                                                class="btn btn-sm btn-outline-secondary">View</a>
+                                                className="btn btn-sm btn-outline-secondary">View</a>
                                             <a href="https://github.com/michellecoder/The-Collector" target="_blank"
-                                                class="btn btn-sm btn-outline-secondary">Repo</a>
+                                                className="btn btn-sm btn-outline-secondary">Repo</a>
                                         </div>
                                     </div>
                                 </div>
@@ -116,22 +117,22 @@ export default function Projects() {
                         </div>
 
                         {/* Weather Dashboard */}
-                        <div class="col">
-                            <div class="card shadow">
-                                <img src="./assets/images/weathdash.jpg" class="bd-placeholder-img card-img-top"
+                        <div className="col">
+                            <div className="card shadow">
+                                <img src="./assets/images/weathdash.jpg" className="bd-placeholder-img card-img-top"
                                     width="100%" height="225" alt="Weather Dashboard Thumbnail" />
 
-                                <div class="card-body">
-                                    <p class="card-text"><strong>Weather Dashboard</strong> - Search a city and see its
+                                <div className="card-body">
+                                    <p className="card-text"><strong>Weather Dashboard</strong> - Search a city and see its
                                         current weather and 5-Day forecast.</p>
-                                    <p class="card-text"><strong>Technologies Used</strong> - HTML, CSS, JavaScript, jQuery
+                                    <p className="card-text"><strong>Technologies Used</strong> - HTML, CSS, JavaScript, jQuery
                                     </p>
-                                    <div class="d-flex justify-content-center align-items-center">
-                                        <div class="btn-group">
+                                    <div className="d-flex justify-content-center align-items-center">
+                                        <div className="btn-group">
                                             <a href="https://adriancronin.github.io/WeatherDashboard/" target="_blank"
-                                                class="btn btn-sm btn-outline-secondary">View</a>
+                                                className="btn btn-sm btn-outline-secondary">View</a>
                                             <a href="https://github.com/AdrianCronin/WeatherDashboard" target="_blank"
-                                                class="btn btn-sm btn-outline-secondary">Repo</a>
+                                                className="btn btn-sm btn-outline-secondary">Repo</a>
                                         </div>
                                     </div>
                                 </div>
@@ -139,29 +140,29 @@ export default function Projects() {
                         </div>
 
                         {/* Work Day Scheduler */}
-                        <div class="col">
-                            <div class="card shadow">
-                                <img src="./assets/images/worksched.jpg" class="bd-placeholder-img card-img-top"
+                        <div className="col">
+                            <div className="card shadow">
+                                <img src="./assets/images/worksched.jpg" className="bd-placeholder-img card-img-top"
                                     width="100%" height="225" alt="Work Day Scheduler Thumbnail" />
-                                <div class="card-body">
-                                    <p class="card-text"><strong>Work Day Scheduler</strong> - A tool to help organize and
+                                <div className="card-body">
+                                    <p className="card-text"><strong>Work Day Scheduler</strong> - A tool to help organize and
                                         plan your day.</p>
-                                    <p class="card-text"><strong>Technologies Used</strong> - HTML, CSS, JavaScript, jQuery
+                                    <p className="card-text"><strong>Technologies Used</strong> - HTML, CSS, JavaScript, jQuery
                                     </p>
-                                    <div class="d-flex justify-content-center align-items-center">
-                                        <div class="btn-group">
+                                    <div className="d-flex justify-content-center align-items-center">
+                                        <div className="btn-group">
                                             <a href="https://adriancronin.github.io/Work-Day-Scheduler/" target="_blank"
-                                                class="btn btn-sm btn-outline-secondary">View</a>
+                                                className="btn btn-sm btn-outline-secondary">View</a>
                                             <a href="https://github.com/AdrianCronin/Work-Day-Scheduler" target="_blank"
-                                                class="btn btn-sm btn-outline-secondary">Repo</a>
+                                                className="btn btn-sm btn-outline-secondary">Repo</a>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
 
-                    </div>
-                </div>
+                    </Row>
+                </Container>
             </div>
 
         </main>
