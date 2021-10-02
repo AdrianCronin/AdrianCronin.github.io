99d309111515f5d41a4b5bb87476a97c77e48ddd
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-10-02 09:56:54 -0700

    converted contact component to react jsx

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,19 +2,38 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 
 export default function Contact() {
     return (
-        <div className="col-md-6" id="contact">
-            <div className="h-100 p-5 text-white bg-dark rounded-3">
-                <h2>Contact Me</h2>
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#contact"><h2>Contact Me</h2></Navbar.Brand>
                 <p className="text-secondary">Let's work together</p>
-                <a href="mailto:acronindev@gmail.com" className="btn btn-outline-secondary" target="_blank">Email:
-                    acronindev@gmail.com</a>
-                <a href="https://github.com/AdrianCronin" className="btn btn-outline-secondary" target="_blank">GitHub</a>
-                <a href="https://www.linkedin.com/in/adrian-cronin-4442b5216/" className="btn btn-outline-secondary"
-                    target="_blank">LinkedIn</a>
-            </div>
-        </div>
+                <Nav className="">
+                    <Nav.Link href="mailto:acronindev@gmail.com" className="btn btn-outline-secondary" target="_blank">Email:
+                        acronindev@gmail.com</Nav.Link>
+                    <Nav.Link href="https://github.com/AdrianCronin" className="btn btn-outline-secondary" target="_blank">GitHub</Nav.Link>
+                    <Nav.Link href="https://www.linkedin.com/in/adrian-cronin-4442b5216/" className="btn btn-outline-secondary"
+                        target="_blank">LinkedIn</Nav.Link>
+
+                </Nav>
+            </Container>
+        </Navbar>
     );
 };
+
+{/* <Col className="col-md-6" id="contact">
+<Container className="h-100 p-5 text-white bg-dark rounded-3">
+    <h2>Contact Me</h2>
+    <p className="text-secondary">Let's work together</p>
+    <Nav>
+        <Nav.Link href="mailto:acronindev@gmail.com" className="btn btn-outline-secondary" target="_blank">Email:
+            acronindev@gmail.com</Nav.Link>
+        <Nav.Link href="https://github.com/AdrianCronin" className="btn btn-outline-secondary" target="_blank">GitHub</Nav.Link>
+        <Nav.Link href="https://www.linkedin.com/in/adrian-cronin-4442b5216/" className="btn btn-outline-secondary"
+            target="_blank">LinkedIn</Nav.Link>
+    </Nav>
+</Container>
+</Col> */}
