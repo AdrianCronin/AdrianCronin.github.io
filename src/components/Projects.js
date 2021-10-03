48c2f234259049fa874457d4fb096aa9e3758582
Adrian B. Cronin <cronin.adrianb@gmail.com>
2021-10-02 17:16:27 -0700

    removed old cup of sugear card

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -35,29 +35,6 @@ export default function Projects() {
                                 repo={project.repo}
                             />
                         ))}
-
-                        {/* Cup of Sugear */}
-                        {/* <Col>
-                            <Card className="shadow position-relative">
-                                <Card.Img variant="top" src="./assets/images/cupofsugear.png" width="100%" height="225" alt="Cup of Sugar Thumbnail" />
-
-                                <Card.Body >
-                                    <Card.Text><strong>Cup of Sugear</strong> - A tool to help neighbors
-                                        borrow/loan/exchange items.</Card.Text>
-                                    <Card.Text><strong>Technologies Used</strong> - HTML, CSS, JavaScript,
-                                        Node.js, Express.js, MySQL, Sequelize, Handlebars</Card.Text>
-                                    <div className="d-flex justify-content-center align-items-center">
-                                        <ButtonGroup>
-                                            <a rel="noreferrer" href="https://cup-of-sugear.herokuapp.com/" target="_blank"
-                                                className="btn btn-sm btn-outline-secondary">View</a>
-                                            <a rel="noreferrer" href="https://github.com/AdrianCronin/Cup-of-Sugear" target="_blank"
-                                                className="btn btn-sm btn-outline-secondary">Repo</a>
-                                        </ButtonGroup>
-                                    </div>
-                                </Card.Body>
-                            </Card>
-                        </Col> */}
-
                         {/* Tech Blog */}
                         <div className="col">
                             <div className="card shadow position-relative">
