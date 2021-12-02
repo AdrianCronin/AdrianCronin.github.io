44654b34d1afbb3c51c61605d6a5a942eeb2a058
Adrian-PC <cronin.adrianb@gmail.com>
2021-12-01 21:10:04 -0800

    added material drawer

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,13 @@ import Navtab from '../components/Navtab';
 import About from '../components/About';
 import Projects from '../components/Projects';
 import Contact from '../components/Contact';
+import PortfolioDrawer from '../components/Drawer';
 
 export default function Home() {
 
     return (
         <>
+            <PortfolioDrawer/>
             <Navtab />
             <About />
             <Projects />
