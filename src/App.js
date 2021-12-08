7c228ee35c165a28309f9228e16f542c4f255676
Adrian-PC <cronin.adrianb@gmail.com>
2021-12-07 16:55:56 -0800

    removed app.css and moved its contents to  index.css

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 
-import './App.css';
-
 function App() {
   return (
     <div className="App bg-dark">
