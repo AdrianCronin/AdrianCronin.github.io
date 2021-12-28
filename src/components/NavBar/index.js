63c74a2070fcf57ed529a2a3da94906056059136
Adrian-PC <cronin.adrianb@gmail.com>
2021-12-28 12:52:51 -0800

    resetting direction

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -2,15 +2,13 @@
 export default function Navbar() {
 
     return (
-        <nav className='flex navbar'>
-            {/* <div>Adrian Cronin</div> */}
-            <ul className='flex nav-list'>
-                <li><a href="#">About Me</a></li>
-                <li><a href="#">Projects</a></li>
-                <li><a href="#">Contact</a></li>
-                <li><a href="#">Resume</a></li>
+        <nav role="navigation">
+            <ul id="menu">
+                <a href="#"><li>About Me</li></a>
+                <a href="#"><li>Projects</li></a>
+                <a href="#"><li>Contact</li></a>
+                <a href="#"><li>Resume</li></a>
             </ul>
         </nav>
-    )
-
-}
+    );
+};
